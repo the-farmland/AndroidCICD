9addab53c362ddf986f871819a9d1c8821c59b71
the-farm <167935056+the-farmland@users.noreply.github.com>
2024-11-22 23:44:14 +1200

    Update MainActivity.kt

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,49 +1,26 @@
 package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import android.widget.EditText
 
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+
 class MainActivity : AppCompatActivity() {
-    private lateinit var webView: WebView
-    private lateinit var editText: EditText
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        webView = findViewById(R.id.webView)
-        editText = findViewById(R.id.editText)
-
-        // Configure WebView
-        webView.settings.apply {
-            javaScriptEnabled = true
-            loadWithOverviewMode = true
-            useWideViewPort = true
-        }
-        webView.webViewClient = WebViewClient()
-        
-        // Load a default page
-        webView.loadUrl("https://www.google.com")
-    }
-
-    fun loadUrl(view: View) {
-        var url = editText.text.toString()
-        // Add https:// if not present
-        if (!url.startsWith("http://") && !url.startsWith("https://")) {
-            url = "https://$url"
-        }
-        webView.loadUrl(url)
-    }
-
-    override fun onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
-        }
-    }
+	/** Called when the user taps the Send button */
+	fun sendMessage(@Suppress("UNUSED_PARAMETER") view: View) {
+		val editText = findViewById<EditText>(R.id.editText)
+		val message = editText.text.toString()
+		val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+			putExtra(EXTRA_MESSAGE, message)
+		}
+		startActivity(intent)
+	}
 }
